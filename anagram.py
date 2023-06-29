6565db199244a2c8874132dcd6beacfe7b88295d
Saher Scafi <95544378+sscafi@users.noreply.github.com>
2023-06-29 15:23:22 -0400

    Add files via upload

--- anagram.py
+++ anagram.py
@@ -1,27 +1,10 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        # Convert both strings to lowercase and remove whitespace
-        s = s.lower().replace(" ", "")
-        t = t.lower().replace(" ", "")
-
-        # Check if the lengths of the strings are equal
-        if len(s) != len(t):
-            return False
-
-        # Create dictionaries to count the frequency of each character
-        count_s = {}
-        count_t = {}
-
-        # Count the frequency of characters in s
-        for char in s:
-            count_s[char] = count_s.get(char, 0) + 1
-
-        # Count the frequency of characters in t
-        for char in t:
-            count_t[char] = count_t.get(char, 0) + 1
-
-        # Compare the dictionaries
-        if count_s == count_t:
-            return True
-        else:
-            return False
+    def reverse(self, x: int) -> int:
+            if x >= 0:
+                y = int(str(x)[::-1])
+                return y if y < 2147483648 else 0
+        
+            else: 
+                y = -int(str(x)[:0:-1])
+                return y if y > -2147483648 else 0
+        
